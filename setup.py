6bd8e87c8a76c9af574a80b3f1067736a8288303
Alexandre Janin <alexandre.janin@protonmail.com>
2026-02-11 17:28:43 +0100

    f2py and Meson build
    New build system for the fortran source code

--- setup.py
+++ setup.py
@@ -1,35 +1,31 @@
-from os.path import join
-import numpy as np
+from setuptools import setup, find_packages
 
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    
-    # Get the numpy direction
-    nppath=np.__path__[0]
-    npinclude = join(nppath, 'core/include/numpy')
-
-    # Create a config object
-    config = Configuration('py3Ddef', parent_package, top_path)
-    
-    # Where are the includes
-    include_dirs = ['src/', npinclude]
-    
-    # Which are the sources
-    sources = ['src/3dmain.f', 'src/okada_sub.f', 'src/xyz_output.f']
-
-    # Additional flags
-    CFLAGS = []
-
-    # Create an extension
-    config.add_extension('_all3Ddef', 
-            sources=sources, 
-            include_dirs=include_dirs,
-            extra_compile_args=CFLAGS)
-
-    # All done
-    return config
-
-# Main
-if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(configuration=configuration, version='1.0.0')
+setup(
+    name="py3Ddef",
+    version="1.1.0",
+    author='Alexandre JANIN',
+    author_email='alexandre.janin@protonmail.com',
+    url='https://github.com/AlexandrePFJanin/py3Ddef',
+    description='Python implementation of the three-dimensional, boundary element modeling code 3D~def.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    package_data={
+        "py3Ddef": ["_all3Ddef*.so"],
+    },
+    zip_safe=False,
+    install_requires=[
+        'ipython>=8.15.0',
+        'numpy>=1.12',
+        'matplotlib>=3.0',
+        'termcolor',
+        'h5py'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
